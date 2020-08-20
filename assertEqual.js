2bde3aa5a1228651b014b4bd03640e8663eae60f
Kaliza Anna <annkaliza@gmail.com>
2020-08-20 02:56:41 +0000

    A function will return the first item of the array passed as argument added to head.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,32 +1,4 @@
 
-
-// const assertEqual = function(actual, expected) {
-//   let passEmoji = String.fromCodePoint(0x1F44C);
-//   let failEmoji = String.fromCodePoint(0x1F4A5);
-
-//   if (typeof actual === "string") {
-
-//     actual = `"${actual}"`;
-    
-//   }
-
-//   if (typeof expected === 'string') {
-//     expected = `"${expected}"`;
-//   }
-
-//   if (actual === expected) {
-//     console.log(`${passEmoji} Assertion passed: ${actual} == ${expected}`);
-//   } else {
-//     console.log(`${failEmoji} Assertion failed: ${actual} != ${expected}`);
-//   }
-
-// };
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual(1, "1");
-// assertEqual("bootcamp", "Bootcamp");
-
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
