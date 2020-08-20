8db8d53631c9d633dfec0b073ef98ddace2469dc
Kaliza Anna <annkaliza@gmail.com>
2020-08-20 02:36:21 +0000

    Returns the first item of the array passed as an argument head.js added

--- head.js
+++ head.js
@@ -9,7 +9,7 @@ const assertEqual = function(actual, expected) {
 
 const head = function(array) {
   return array[0];
-  // should return the first item of the array passed as argument
+  // will return the first item of the array passed as argument
 };
 
 // TEST CODE
