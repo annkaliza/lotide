beeb454b76c91d525ddb99bd851738b2b6afd157
Kaliza Anna <annkaliza@gmail.com>
2020-08-26 20:17:00 +0000

    added assertArraysEqualTest file

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([2,1,3],[2,1,3]);
+assertArraysEqual(['hello', 'hi'],['helo','hi']);
+assertArraysEqual([1,2,3],[1,2]);
+assertArraysEqual(['Lighthouse Labs'],['Lighthouse Labs']);
+assertArraysEqual(["h","a"],["H","a"]);
