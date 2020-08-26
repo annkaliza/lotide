29c3399eaf5a4e9f00783fc2b6ac7d83880283c0
Kaliza Anna <annkaliza@gmail.com>
2020-08-26 20:26:18 +0000

    add middleTest file

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle= require('../middle');
+
+assertArraysEqual(middle([1,2,3]),[2]);
+assertArraysEqual(middle([1,2,3,4]),[2,3]);
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([1,2]),[]);
+assertArraysEqual(middle([]),[]);
+assertArraysEqual(middle(["1","Hello","Lighthouse"]),["Hello"]);
+assertArraysEqual(middle([1,2,3,4,5,6,7,8,9]),[5]);
+assertArraysEqual(middle([1,2,3,4,5,6,7,8,9,10]),[5,6 ]);
+assertArraysEqual(middle([1,2,3,4,5,6]),[3,4]);
